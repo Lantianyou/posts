9a8728d3c04e374b504102668a9816ec5d3cd5d1
Lantianyou <dtlantianyou@gmail.com>
2025-03-04 13:37:46 +0800

    feat: have a backlink

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
+import Link from "next/link";
 
 export default function Index() {
   const allPosts = getAllPosts();
@@ -25,6 +26,18 @@ export default function Index() {
         />
         {morePosts.length > 0 && <MoreStories posts={morePosts} />}
       </Container>
+      <footer className="py-10 bg-gradient-to-r   text-center mt-10">
+        <div className="container mx-auto px-4">
+          <p className="text-xl font-bold mb-2">
+            Building{" "}
+            <Link href="https://fluxanime.com" className="hover:underline" target="_blank" rel="noopener noreferrer">
+              Flux Anime
+            </Link>
+          </p>
+          <p className="text-sm opacity-80">Dedicated to building a virtual world</p>
+          <p className="mt-4 text-xs">&copy; {new Date().getFullYear()} Flux Anime. All rights reserved.</p>
+        </div>
+      </footer>
     </main>
   );
 }
